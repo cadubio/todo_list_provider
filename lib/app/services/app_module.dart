443a3467afeece53530ac910efedc73dd8445858
cadubio <cadu@e.email>
2023-07-02 22:42:40 -0300

    Ad. User Service

--- lib/app/services/app_module.dart
+++ lib/app/services/app_module.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_connection_factory.dart';
+import 'package:todo_list_provider/app/repositories/user/user_repository_impl.dart';
 import 'package:todo_list_provider/app/services/app_widget.dart';
 
 /// Estrutura de Módulos:
@@ -17,10 +19,16 @@ class AppModule extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        Provider(create: (_) => FirebaseAuth.instance),
         Provider(
           create: (_) => SqliteConnectionFactory(),
           lazy: false, // instancia a classe assim que abrir o App
-        )
+        ),
+        Provider(
+          create: (context) => UserRepositoryImpl(
+            firebaseAuth: context.read(),
+          ),
+        ),
       ],
       child: const AppWidget(),
     );
